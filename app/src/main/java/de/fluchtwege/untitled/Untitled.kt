a11b009b2e6326d63f4985b7de6b533dd5ae8a0f
Tristan Hubel <tristan.hubel@magicline.de>
2017-08-03 15:34:45 +0200

    change layout , prepare for objectbox

--- app/src/main/java/de/fluchtwege/untitled/Untitled.kt
+++ app/src/main/java/de/fluchtwege/untitled/Untitled.kt
@@ -6,11 +6,14 @@ import de.fluchtwege.untitled.di.AppComponent
 import de.fluchtwege.untitled.di.AppModule
 import de.fluchtwege.untitled.di.DaggerAppComponent
 import de.fluchtwege.untitled.persistance.UntitledDatabase
+import io.objectbox.rx.RxBoxStore
 
 class Untitled : Application() {
 
     lateinit var application: Untitled
     lateinit var database: UntitledDatabase
+    lateinit var boxStore: RxBoxStore
+                 private set
 
     companion object {
         lateinit var appComponent: AppComponent
@@ -26,6 +29,9 @@ class Untitled : Application() {
 
     private fun initRoom() {
         database = Room.databaseBuilder(this, UntitledDatabase::class.java, "untitled_database").build()
+    }
+
+    private fun initObjectBox(){
     }
 
     private fun initDagger() {
